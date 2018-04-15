e23eab0324fa53d96625274341d5bf133dd94328
Julia Lidstrom <juleslidstrom@gmail.com>
2018-04-15 18:40:41 -0500

    Working axios and displayed results

--- client/src/components/List/ListItem.js
+++ client/src/components/List/ListItem.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Col, Row, Container } from "../../components/Grid";
 
 export const ListItem = props => (
   <li className="list-group-item">
-    {props.children}
+  <Container>
+      <Row>
+      
+        <Col size="xs-8 sm-9">
+         
+          {/* <p>Ingredients: {props.ingredients}</p> */}
+          <a rel="noreferrer noopener" target="_blank" href={props.href}>
+           <h3>{props.title}</h3>
+          </a>
+        </Col>
+      </Row>
+    </Container>
   </li>
 );
